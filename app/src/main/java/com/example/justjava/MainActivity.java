19ddb4dedaefa47fc9edb2b2b5b92ec02f6bb1b8
Razik <azikblaze@outlook.com>
2019-05-14 15:05:52 -0400

    refactor: Make explicit string conversion in displayQuantity

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -126,7 +126,7 @@ public class MainActivity extends AppCompatActivity {
      */
     private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
+        quantityTextView.setText(Integer.toString(number));
     }
 
     /**
